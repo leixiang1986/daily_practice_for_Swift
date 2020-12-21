b2ea0589462b3513e867ce649a4189f525adcfe3
leixiang <leixiang2014@163.com>
2020-12-21 17:22:53 +0800

    Initial Commit

---
+++ SwiftFunctions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftFunctions
+//
+//  Created by mac on 2020/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
